a8086c7d16dfe66ddcaad6a1ecd14ed3ecb04c4b
BaptisteLEBERRE <73174593+BaptisteLEBERRE@users.noreply.github.com>
2021-05-07 11:38:22 +0200

    Update tsp_graph_init.py

--- tsp_graph_init.py
+++ tsp_graph_init.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 class Lieu:
     def __init__(self):
@@ -44,16 +45,32 @@ class Graph:
     
     def calcul_matrice_cout_od(self, liste_lieux):
         liste_lieux_2 = liste_lieux.copy()
-        matrice_distances = []
+        matrice_od = []
         for lieu in liste_lieux:
             liste_lieux_2.remove(lieu)
             for lieu_2 in liste_lieux_2:
                 distance = Lieu().calc_distance(lieu, lieu_2)
                 dict_distance = {"l1":lieu["nom"], "l2":lieu_2["nom"], "distance":distance}
-                matrice_distances.append(dict_distance)
-        return matrice_distances
+                matrice_od.append(dict_distance)
+        return matrice_od
 
+    def plus_proche_voisins(self, matrice, lieu):
+        lieu = str(lieu)
+        liste_dicts = []
+        for item in matrice:
+            if item["l1"] == lieu:
+                liste_dicts.append({"lieu":item["l2"], "distance":item["distance"]})
+            elif item["l2"] == lieu:
+                liste_dicts.append({"lieu":item["l1"], "distance":item["distance"]})
+                
+        plus_petite_distance = min([dico["distance"] for dico in liste_dicts])
+        plus_proche_voisins = []
+        for dico in liste_dicts:
+            if dico["distance"] == plus_petite_distance:
+                plus_proche_voisins.append(dico)
+        return plus_proche_voisins
+        
 
 liste_lieux = Graph().liste_lieux(10, 20, 4)
-matrice_distances = Graph().calcul_matrice_cout_od(liste_lieux)
-print(matrice_distances)
+matrice_od = Graph().calcul_matrice_cout_od(liste_lieux)
+print(Graph().plus_proche_voisins(matrice_od, "lieu 1"))
