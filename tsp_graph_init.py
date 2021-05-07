6cd7f0f6b2118734b152ad421ea3f64e83960b93
LueMar-R <ludivine.maintier@isen-ouest.yncrea.fr>
2021-05-07 12:26:30 +0200

    maj midi

--- tsp_graph_init.py
+++ tsp_graph_init.py
@@ -4,15 +4,47 @@ import time
 import pandas as pd
 
 class Lieu:
-    def distance(self, lieu1, lieu2):
-        pass
+    @classmethod
+    def def_lieu(cls, x, y):
+        return (float(x), float(y))
+
+    @classmethod
+    def distance(cls, lieu1, lieu2):
+        return np.sqrt((lieu1[0]-lieu2[0])**2 + (lieu1[1]-lieu2[1])**2)
+
 
 class Graph:
-    def __init__(self, LARGEUR, HAUTEUR, NB_LIEUX):
-        self.LARGEUR = None
-        self.HAUTEUR = None
-        self.NB_LIEUX = None
-        liste_lieux = []
+
+    @classmethod
+    def creer_liste_lieux(cls):
+        cls.LARGEUR = 6
+        cls.HAUTEUR = 10
+        cls.NB_LIEUX = 4
+        cls.liste_lieux = []
+        for i in range(cls.NB_LIEUX):
+            x = random.uniform(0, cls.LARGEUR)
+            y = random.uniform(0, cls.HAUTEUR)
+            cls.liste_lieux.append([x,y])
+        return cls.liste_lieux
+
+    @classmethod
+    def calcul_matrice_cout_od(cls, liste_lieux):
+        cls.LEN = len(liste_lieux)
+        cls.matrice_od = np.zeros((cls.LEN, cls.LEN))
+        for i in range(cls.LEN):
+            for j in range(cls.LEN):
+                if i == j:
+                    cls.matrice_od[i,j] = np.inf
+                if i != j:
+                    cls.matrice_od[i,j] = Lieu.distance(liste_lieux[i], liste_lieux[j])
+        return cls.matrice_od
+        
+    @classmethod    
+    def plus_proche_voisin(cls, lieu, matrice_od) :
+        print(matrice_od)
+        cls.le_plus_proche_voisin = np.min(matrice_od[lieu])
+        return cls.le_plus_proche_voisin
+
 
     @classmethod
     def charger_graph(cls):
@@ -22,16 +54,21 @@ class Graph:
     def sauvegarder_graph(cls):
         pass
 
-    def calcul_matrice_cout_od(self):
-        matrice_od = null
-        pass
-    
-    def plus_proche_voisin(self) :
-        pass
-
 class Route:
     def __init__(self):
         pass
 
 class Affichage:
     pass
+
+
+l_lieux = Graph.creer_liste_lieux()
+print(l_lieux)
+
+matrice_cout = Graph.calcul_matrice_cout_od(l_lieux)
+print(matrice_cout)
+
+voisin_1 = Graph.plus_proche_voisin(1, matrice_cout)
+voisin_2 = Graph.plus_proche_voisin(2, matrice_cout)
+print(voisin_1)
+print(voisin_2)
