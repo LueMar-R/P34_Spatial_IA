65a3136ae83c315e503ea2f86badfb08ac5f8927
LueMar-R <ludivine.maintier@isen-ouest.yncrea.fr>
2021-05-07 09:48:55 +0200

    test

---
+++ tsp_graph_init.py
@@ -0,0 +1,24 @@
+import numpy as np
+import random
+import time
+import pandas as pd
+
+class Lieu:
+    def distance(self, lieu1, lieu2):
+        pass
+
+class Graph:
+    liste_lieux = None
+    LARGEUR = None
+    HAUTEUR = None
+    NB_LIEUX = None
+
+    def calcul_matrice_cout_od(self):
+        pass
+
+class Route:
+    def __init__(self):
+        pass
+
+class Affichage:
+    pass
