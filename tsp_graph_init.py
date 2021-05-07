ed7f02eeae46224f57806e41ee8c2943342a5e29
LueMar-R <ludivine.maintier@isen-ouest.yncrea.fr>
2021-05-07 10:06:21 +0200

    commit1

--- tsp_graph_init.py
+++ tsp_graph_init.py
@@ -8,12 +8,25 @@ class Lieu:
         pass
 
 class Graph:
-    liste_lieux = None
-    LARGEUR = None
-    HAUTEUR = None
-    NB_LIEUX = None
+    def __init__(self, LARGEUR, HAUTEUR, NB_LIEUX):
+        self.LARGEUR = None
+        self.HAUTEUR = None
+        self.NB_LIEUX = None
+        liste_lieux = []
+
+    @classmethod
+    def charger_graph(cls):
+        pass
+
+    @classmethod
+    def sauvegarder_graph(cls):
+        pass
 
     def calcul_matrice_cout_od(self):
+        matrice_od = null
+        pass
+    
+    def plus_proche_voisin(self) :
         pass
 
 class Route:
