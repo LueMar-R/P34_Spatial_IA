52761dbca7d8cfbd78d7f15d10865b3fe8f07f41
RexiaEva <Eva.Moulard@laposte.net>
2021-05-10 16:11:15 +0200

    ....

--- Graph.py
+++ Graph.py
@@ -15,22 +15,50 @@ class Graph:
         self.liste_lieux = numpy.empty((0, nb_l))
 
         for i in range(0, nb_l - 1):
-            self.liste_lieux[i] = Lieu.Lieu(randint(0, l), randint(0, h))
+            self.liste_lieux[i] = Lieu.Lieu(random.randint(0, l), random.randint(0, h))
 
-        def calcul_matrice_cout_od(self):
-            for i in self.liste_lieux:
-                for j in self.liste_lieux:
-                    Graph.matrice_od.append({'LieuA' : i.id, 'LieuB' : j.id, 'Distance' : i.distance(j) })
+    def __repr__(self):
+        return "<Graph : nombre de lieu = {}, largeur = {}, hauteur = {}>".format(self.NB_LIEUX, self.LARGEUR, self.HAUTEUR)
 
-        def plus_proche_voisin(self, L):
-            voisins = Graph.matrice_od[Graph.matrice_od['LieuA'] == L.id]
-            ligne_proche = voisins[voisins['Distance'] == voisins['Distance'].min()]
-            return Lieu.Lieu.dLieux[ligne_proche['LieuB']]
-        
-        def charger_graph(self, fichier):
-            df = pandas.read_csv(fichier)
-            return df.values
+    def __str__(self):
+        return "Nombre de lieu = {}, Largeur = {}, Hauteur = {}\n{}".format(self.NB_LIEUX, self.LARGEUR, self.HAUTEUR, self.liste_lieux)
+    
+    def calcul_matrice_cout_od(self):
+        """calculer une matrice de distances entre chaque lieu du graphe"""
+        for i in self.liste_lieux:
+            for j in self.liste_lieux:
+                Graph.matrice_od.append({'LieuA' : i, 'LieuB' : j, 'Distance' : i.distance(j) })
 
-        def sauvegarder_graph(self, fichier):
-            df = pandas.DataFrame(self.liste_lieux)
-            df.to_csv(fichier)
+    def plus_proche_voisin(self, L, reste = self.liste_lieux):
+        """renvoyer le plus proche voisin d'un lieu"""
+        voisins = Graph.matrice_od[Graph.matrice_od['LieuA'] == L]
+        ligne_proche = voisins[voisins['Distance'] == voisins['Distance'].min()]
+        return Lieu.Lieu.dLieux[ligne_proche['LieuB']]
+    
+    def charger_graph(self, fichier):
+        df = pandas.read_csv(fichier)
+        return df.values
+
+    def sauvegarder_graph(self, fichier):
+        df = pandas.DataFrame(self.liste_lieux)
+        df.to_csv(fichier)
+
+if __name__ == "__main__":
+    from matplotlib import pyplot as plt
+
+    lt = 20
+    ht = 20
+    nb = 5
+
+    gt = Graph(nb, lt, ht)
+
+    print(gt)
+
+    
+
+    pt1 = Lieu(xt1, yt1)
+
+    print("{}".format(pt1))
+
+    plt.plot(pt1.x, pt1.y, marker="o", color="blue")
+    plt.show()
--- Lieu.py
+++ Lieu.py
@@ -1,20 +1,19 @@
 class Lieu:
     """Cette classe sert à mémoriser les coordonnées x et y du lieu à visiter."""
-    dLieux = {} # liste complète des lieux sous forme de dictionnaire
-    a_id = 0 # id disponible actuelle
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.id = Lieu.a_id
-        Lieu.dLieux[Lieu.a_id] = self
-        Lieu.a_id+=1
 
     def __repr__(self):
-        return "<Lieu : id = {}, x = {}, y = {}>".format(self.id, self.x, self.y)
+        return "<Lieu : x = {}, y = {}>".format(self.x, self.y)
 
     def __str__(self):
         return "({},{})".format(self.x, self.y)
+
+    def __eq__(self, b):
+        self.x == b.x
+        self.y == y
     
     def distance(self, b):
         """fonction de calcul de distance euclidienne entre 2 lieux."""
@@ -24,12 +23,12 @@ if __name__ == "__main__":
     import random
     from matplotlib import pyplot as plt
 
-    xt1 = random.randint(-20, 20)
-    yt1 = random.randint(-20, 20)
+    xt = random.randint(-20, 20)
+    yt = random.randint(-20, 20)
 
-    pt1 = Lieu(xt1, yt1)
+    pt = Lieu(xt, yt)
 
-    print("{}".format(pt1))
+    print(pt1)
 
     plt.plot(pt1.x, pt1.y, marker="o", color="blue")
     plt.show()
