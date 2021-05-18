0d5da61d0881a4f6092af9a5a3499280d0b7733b
RexiaEva <Eva.Moulard@laposte.net>
2021-05-18 13:53:10 +0200

    ....

---
+++ Affichage.py
@@ -0,0 +1,34 @@
+from Graph import Graph
+from tkinter import *
+
+LARGEUR = 800
+HAUTEUR = 800
+
+class Interface(Frame):
+    
+    """Notre fenêtre principale.
+    Tous les widgets sont stockés comme attributs de cette fenêtre."""
+    
+    def __init__(self, fenetre, **kwargs):
+        Frame.__init__(self, fenetre, width=LARGEUR, height=800, **kwargs)
+        fenetre.title("groupe 6")
+
+        self.graph = Graph(20, LARGEUR, HAUTEUR)
+        rayonPoints = 10
+        
+        self.canvas = Canvas(self, width=LARGEUR, height=HAUTEUR, bg="white")
+        self.canvas.pack()
+        for i, lieu in enumerate(self.graph.liste_lieux):
+            self.canvas.create_oval((lieu.x - rayonPoints, lieu.y - rayonPoints), (lieu.x + rayonPoints, lieu.y + rayonPoints), 
+                            fill="silver", outline="black", width=3)
+            self.canvas.create_text(lieu.x, lieu.y, text=str(i),
+                            fill="black", font= ("courier", 10, "bold italic"),
+                            anchor="center", justify= "center")
+        self.evol = Label(self, text="{}".format(self.graph))
+        self.evol.pack()
+
+fenetre = Tk()
+interface = Interface(fenetre)
+
+interface.mainloop()
+interface.destroy()
